14a5452197e407facb899c2f3b803a9e31445d03
Arshad3721 <124030789+Arshad3721@users.noreply.github.com>
2023-11-07 13:59:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -44,7 +44,6 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://faheem:faheem@faheemansari.ljh4xay.mongodb.net/?retryWrites=true&w=majority")
-")
 DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_fndkdiles')
 
